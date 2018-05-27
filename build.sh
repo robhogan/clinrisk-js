928f48142a3f499a279885bd6da13718ff34f3f1
rh389 <roberthogan@blueyonder.co.uk>
2018-05-27 15:58:01 +0100

    Complete build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# yarn build
+./node_modules/.bin/tsc -d
 rsync -a --include '*/' --include 'package.json' --exclude '*' modules/ dist/
 rsync -a --include '*/' --include 'README.md' --exclude '*' modules/ dist/
 rsync -a --include '*/' --include 'LICENSE' --exclude '*' modules/ dist/
